fa77d277f68a0e514347c27fc274773ef8daa9ab
abuicke <abuicke@treemetrics.com>
2015-08-06 16:34:38 +0100

    Ready for review before sending to HeadWay for testing

--- src/ie/headway/app/howtodoit/gui/StepLayout.java
+++ src/ie/headway/app/howtodoit/gui/StepLayout.java
@@ -1,18 +1,21 @@
 package ie.headway.app.howtodoit.gui;
 
+import static android.util.TypedValue.COMPLEX_UNIT_SP;
+import static android.view.Gravity.CENTER;
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import static android.widget.LinearLayout.VERTICAL;
 import ie.headway.app.howtodoit.xml.Step;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 public class StepLayout extends Fragment implements Layoutable<LinearLayout> {
@@ -25,11 +28,14 @@ public class StepLayout extends Fragment implements Layoutable<LinearLayout> {
 
 		final TextView text = new TextView(context);
 		text.setText(step.getText());
-		text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 65);
-		text.setGravity(Gravity.CENTER);
+		text.setTextSize(COMPLEX_UNIT_SP, 65);
+		text.setGravity(CENTER);
+		text.setLayoutParams(new LayoutParams(MATCH_PARENT, WRAP_CONTENT));
 		mLayout.addView(text);
 
 		final ImageView image = new ImageView(context);
+		image.setLayoutParams(new LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
+		image.setPadding(0, 0, 0, 15);
 		image.setImageURI(
 				new Uri.Builder().path(step.getImagePath()).build());
 		mLayout.addView(image);

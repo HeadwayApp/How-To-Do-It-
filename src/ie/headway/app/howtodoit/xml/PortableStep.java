e7d8754c797d4712500eda2254fb181883b3ce68
abuicke <abuicke@treemetrics.com>
2015-08-22 17:59:01 +0100

    Portability done ...to be continued
    Portability accomplished as well as I'm going to for now.

--- src/ie/headway/app/howtodoit/xml/PortableStep.java
+++ src/ie/headway/app/howtodoit/xml/PortableStep.java
@@ -1,7 +1,5 @@
 package ie.headway.app.howtodoit.xml;
 
-import java.io.File;
-
 import android.os.Environment;
 
 public class PortableStep extends Step {
@@ -9,26 +7,12 @@ public class PortableStep extends Step {
 	/**
 	 * This constant represents the system dependant path element which PortableStep will resolve.
 	 * */
-	public static final String PORTABLE_PATH_PARENT = "EXTERNAL_STORAGE_DIRECTORY";
+	public static final String PATH_ARTIFACT = "EXTERNAL_STORAGE_DIRECTORY";
 	
-	public PortableStep() {
-		
-	}
-	
-	/**
-	 * @debug remove
-	 * */
-	public PortableStep(String text, String imagePath, String audioPath) {
-		super(text, imagePath, audioPath);
-	}
-	
-	/**
-	 * @debug Changle impl to release quality.
-	 * */
 	@Override
 	public String getImagePath() {
-		return Environment.getExternalStorageDirectory() + File.separator + 
-				"Headway" + File.separator + super.getImagePath().split("Headway")[1];
+		return super.getImagePath().replace(
+				PATH_ARTIFACT, Environment.getExternalStorageDirectory().getPath());
 	}
 	
 }

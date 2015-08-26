44d5e25ff1f1591f5f939d7bd0f4228e4ca8e9ed
abuicke <abuicke@treemetrics.com>
2015-08-26 18:51:24 +0100

    AndroidStudio Migration
    Migrated project to Android Studio. This was done so
    HeadwaySharedLibrary could be exported and used as an *.aar file, but is
    a very goof idea anyway. In fact, Android Studio should have been used
    in the first place although it would have been substantially more
    frustrating so this is good too. Res Ipsa Loquitur.

---
+++ app/src/main/java/ie/headway/app/howtodoit/TaskBreakdownActivity.java
@@ -0,0 +1,79 @@
+package ie.headway.app.howtodoit;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import java.io.File;
+
+import ie.headway.app.howtodoit.gui.StepLayoutFragment;
+import ie.headway.app.xml.Task;
+
+import static ie.headway.app.disk.AppDir.ROOT;
+
+public class TaskBreakdownActivity extends FragmentActivity {
+    
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+	
+	@Override
+	protected void onCreate(final Bundle savedInstanceBundle) {
+		super.onCreate(savedInstanceBundle);		
+		setContentView(R.layout.activity_task_breakdown);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new TaskBreakdownAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+	}
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class TaskBreakdownAdapter extends FragmentStatePagerAdapter {
+    	
+    	final File taskDir = ROOT.getFile(getIntent().getCharSequenceExtra("TASK"));
+    	final File taskFile = new File(taskDir, "task.xml");
+    	
+        public TaskBreakdownAdapter(final FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(final int position) {
+    		final Serializer serializer = new Persister();
+    
+    		Task makeTeaTask;
+    		try {
+    			makeTeaTask = serializer.read(Task.class, taskFile);
+    		}catch(Exception e) {
+    			throw new RuntimeException("Deserialization failed!", e);
+    		}
+    		
+    		return StepLayoutFragment.newInstance(makeTeaTask.getStep(position));
+        }
+
+        /**
+         * TODO This can cause crashes if there are unused images in the imgs directory.
+         * */
+        @Override
+        public int getCount() {
+            return new File(taskDir, "imgs").list().length - 1;
+        }
+    }
+
+}
---
+++ app/src/main/java/ie/headway/app/howtodoit/TaskSelectionActivity.java
@@ -0,0 +1,36 @@
+package ie.headway.app.howtodoit;
+
+import static ie.headway.app.disk.AppDir.ROOT;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TaskSelectionActivity extends ListActivity {
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_task_selection);
+		final ArrayAdapter<String> adapter = 
+				new ArrayAdapter<String>(getBaseContext(), R.layout.task_list_item);
+		adapter.addAll(ROOT.getFile().list());
+		setListAdapter(adapter);
+	}
+
+	@Override
+	protected void onListItemClick (final ListView l, final View v, final int pos, final long id) {
+		final Intent intent = new Intent(getApplicationContext(), TaskBreakdownActivity.class);
+		if(v instanceof TextView) {
+			intent.putExtra("TASK", ((TextView)v).getText());
+			startActivity(intent);
+		}else {
+			Toast.makeText(getApplicationContext(), R.string.cant_start_task, Toast.LENGTH_LONG).show();
+		}
+	}
+
+}

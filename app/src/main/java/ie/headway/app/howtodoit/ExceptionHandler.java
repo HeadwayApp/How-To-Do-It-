28f0bfc2783d4f63cfaf304047565cdac1bc0c0b
abuicke <abuicke@treemetrics.com>
2015-11-10 16:51:57 +0000

    exceptions now written to file

--- app/src/main/java/ie/headway/app/howtodoit/ExceptionHandler.java
+++ app/src/main/java/ie/headway/app/howtodoit/ExceptionHandler.java
@@ -1,12 +1,21 @@
 package ie.headway.app.howtodoit;
 
-import android.util.Log;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
 
-    @Override
-    public void uncaughtException(Thread thread, Throwable ex) {
-        Log.e("headway", "exception occurred", ex);
+  @Override
+  public void uncaughtException(Thread thread, Throwable ex) {
+//  Log.e("headway", "exception occurred", ex);
+    final File logFile = new File("/sdcard/headway.log");
+    try {
+      final PrintStream ps = new PrintStream(logFile);
+      ex.printStackTrace(ps);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
     }
+  }
 
 }
